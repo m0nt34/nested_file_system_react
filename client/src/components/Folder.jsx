39dc13a2b595775477a615c5b5446edf9b5cce98
m0nt34 <116570394+m0nt34@users.noreply.github.com>
2024-05-16 01:41:39 +0400

    second comm

--- client/src/components/Folder.jsx
+++ client/src/components/Folder.jsx
@@ -2,6 +2,18 @@ import React, { useState } from "react";
 
 function Folder({ explorer }) {
   const [expand, setExpand] = useState(false);
+  const [showInput,setShowInput] = useState({
+    visible:false,
+    isFolder:null
+  })
+  const handleNewFolder = (e,isFolder)=>{
+    e.stopPropagation();
+    setExpand(true)
+    setShowInput({
+      visible:true,
+      isFolder
+    })
+  }
   return (
     <>
       {explorer.isFolder ? (
@@ -12,8 +24,20 @@ function Folder({ explorer }) {
             onClick={() => setExpand(!expand)}
           >
             <span>📁{explorer.name}</span>
+            <div>
+              <button onClick={(e)=>handleNewFolder(e, true)}>Folder +</button>
+              <button onClick={(e)=>handleNewFolder(e, false)}>File +</button>
+            </div>
           </div>
           <div style={{ display: expand ? "block" : "none",paddingLeft:25 }}>
+            {
+              showInput.visible && <div className="inputContainer">
+                <span>{showInput.isFolder?'📁':'📄'}</span>
+                <input type="text" className="inputContainer" autoFocus onBlur={()=>{
+                  setShowInput({...showInput,visible:false})
+                }}/>
+              </div>
+            }
             {explorer.items.map((exp, i) => {
               return <Folder key={i} explorer={exp}/>;
             })}
